f5a7ea99e0226c227a9f9fb172686a2d53fbd954
Volodymyr Vladykin <vova@pocketguard.com>
2025-12-07 18:53:26 +0200

    Setup handling static files

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { dirname, join } from "path";
 
 import contactsRouter from "./routes/contactsRouter.js";
 import authRouter from "./routes/authRoutes.js";
-
 
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/auth", authRouter);
